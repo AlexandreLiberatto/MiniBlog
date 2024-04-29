16d6a133332ffc18d7848731ab318f23fc6efda7
unknown <alexandreliberatto@gmail.com>
2024-04-28 23:28:49 -0300

    Resgatando dados do usuário no Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,62 @@
-import './App.css'
+import "./App.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from "./context/AuthContext";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+//hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 // Pages
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 // Components
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // Login
-import Login from './Login/Login'
+import Login from "./Login/Login";
 
 // Register
-import Register from './Register/Register'
+import Register from "./Register/Register";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <div className='App'>
-      <BrowserRouter>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </div>
-      <Footer />
-      </BrowserRouter>
+    <div className="App">
+      <AuthProvider value={user}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
